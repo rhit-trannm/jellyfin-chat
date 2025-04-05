0eef9e40af6a6cdb33570e85acc985e6f7e862cd
Nhan Tran <slippinice@gmail.com>
2025-04-04 21:10:22 -0500

    new

--- Jellyfin.Plugin.JellyfinChat/Plugin.cs
+++ Jellyfin.Plugin.JellyfinChat/Plugin.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using Jellyfin.Plugin.Template.Configuration;
+using Jellyfin.Plugin.JellyfinChat.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
-namespace Jellyfin.Plugin.Template;
+namespace Jellyfin.Plugin.JellyfinChat;
 
 /// <summary>
 /// The main plugin.
@@ -26,10 +26,10 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     }
 
     /// <inheritdoc />
-    public override string Name => "Template";
+    public override string Name => "JellyfinChat";
 
     /// <inheritdoc />
-    public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
+    public override Guid Id => Guid.Parse("b148ad2b-4caa-4ab7-b501-e405940a8f20");
 
     /// <summary>
     /// Gets the current plugin instance.
